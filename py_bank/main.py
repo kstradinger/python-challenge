3bcd7675a1465621e7d04ad0e4598af998b97df6
kstradinger <kevstradinger@gmail.com>
2019-06-07 11:51:42 -0500

    Added avg change for py_bank, cleaned up code, deleted unused variables, and added comments

--- py_bank/main.py
+++ py_bank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import statistics
 
 csvpath = os.path.join('..','python-challenge', 'budget_data.csv')
 
@@ -14,11 +15,14 @@ with open(csvpath, newline="") as csv_file:
     max_profit = 0
     min_profit = 0
     profit_change = 0
+    profit_list =[]
+    profit_change_list = []
 
     for row in csvreader:
         #Define lists for Date & Row column
         date = row[0]
         profit= int(row[1])
+        profit_list.append(profit)
         #Count the total months in data set
         total_months=csvreader.line_num-1
         #sum together all profit rows for net profit
@@ -29,8 +33,14 @@ with open(csvpath, newline="") as csv_file:
         #if then for min profit
         if int(row[1]) < int(min_profit):
             min_profit = row[1]
-        #Calculate change in profit for avg change
-        
+
+# print(profit_list[0])
+list_length = len(profit_list)
+for i in range(list_length-1):
+    profit_change = profit_list[i+1] - profit_list[i]
+    profit_change_list.append(profit_change) 
+#Calculate average change
+average_change= round((statistics.mean(profit_change_list)),2)       
 
 
 #Print out stats
@@ -38,17 +48,20 @@ print('Financial Analysis')
 print('-------------------------------------------------')
 print(f'Total period length: {total_months} Months')        
 print(f'Net profits over the given period: ${net_profit}')
+print(f'Average Change: ${average_change}')
 print(f'Max Monthly Profit:${max_profit}')
 print(f'Min Monthly Profit ${min_profit}')
+print('-------------------------------------------------')
 
 # output text file
-# with open("Financial_analysis.txt", "w+") as text_file:
-#     text_file.write('Financial Analysis')
-#     text_file.write('-------------------------------------------------')
-#     text_file.write(f'Total period length: {total_months} Months')        
-#     text_file.write(f'Net profits over the given period: ${net_profit}')
-#     text_file.write(f'Max Monthly Profit:${max_profit}')
-#     text_file.write(f'Min Monthly Profit ${min_profit}')
+with open("Financial_analysis.txt", "w+") as text_file:
+    text_file.write('Financial Analysis')
+    text_file.write('-------------------------------------------------')
+    text_file.write(f'Total period length: {total_months} Months')        
+    text_file.write(f'Net profits over the given period: ${net_profit}')
+    text_file.write(f'Average Change: ${average_change}')
+    text_file.write(f'Max Monthly Profit:${max_profit}')
+    text_file.write(f'Min Monthly Profit ${min_profit}')
 
 
 
--- py_poll/main.py
+++ py_poll/main.py
@@ -16,6 +16,7 @@ with open(csv_path, newline="") as csv_file:
     total_votes=0
     candidate_list = []
     unique_candidate_list = []
+    maximum_votes = 0
     #begin for loop
     for row in csv_reader:
         #Define columns as list variables
@@ -26,10 +27,12 @@ with open(csv_path, newline="") as csv_file:
         total_votes = total_votes + 1
         #Identify candidates with votes 
         candidate_list.append(row[2])
+
+   #make a list of unique candidate names for 
     for x in candidate_list:
             if x not in unique_candidate_list:
-                unique_candidate_list.append(x) 
-        #vote count per candidate candidate.count('didate_name')
+                unique_candidate_list.append(x)   
+    #calculate vote count and percent for each candidate
     kahn_votes = candidate_list.count("Khan")
     kahn_pct= round((kahn_votes / total_votes)*100,2)
     print(f'Khan: {kahn_votes} Votes ({kahn_pct}%)')
@@ -45,11 +48,25 @@ with open(csv_path, newline="") as csv_file:
     otooley_votes = candidate_list.count("O'Tooley")
     otooley_pct = round((otooley_votes/total_votes)*100,2)
     print(f"O'Tooley:{otooley_votes} Votes ({otooley_pct}%)")
+#Create a list of the vote ct for each candidate to make a winner variable
+vote_list= [kahn_votes, correy_votes,li_votes,otooley_votes]
+winner_vote_count = max(vote_list)
+if winner_vote_count == kahn_votes:
+    winning_candidate = "Kahn"
+elif winner_vote_count == correy_votes:
+    winning_candidate = "Correy"
+elif winner_vote_count == li_votes:
+    winning_candidate = "Li"
+else :
+    winning_candidate = "O'Tooley"
+print(f'The winner of the election, with {winner_vote_count} votes is: {winning_candidate}')
 
-# with open("poll_results.txt","w+") as text_file:
-#     text_file.write('Election Results')
-#     text_file.write('----------------')
-#     text_file.write(f'Khan: {kahn_votes} Votes ({kahn_pct}%)')
-#     text_file.write(f'Correy:{correy_votes} Votes ({correy_pct}%)')
-#     text_file.write(f'Li:{li_votes} Votes ({li_pct}%)')
-#     text_file.write(f"O'Tooley:{otooley_votes} Votes ({otooley_pct}%)")
+#Print text file with results
+with open("poll_results.txt","w+") as text_file:
+    text_file.write('Election Results')
+    text_file.write('----------------')
+    text_file.write(f'Khan: {kahn_votes} Votes ({kahn_pct}%)')
+    text_file.write(f'Correy:{correy_votes} Votes ({correy_pct}%)')
+    text_file.write(f'Li:{li_votes} Votes ({li_pct}%)')
+    text_file.write(f"O'Tooley:{otooley_votes} Votes ({otooley_pct}%)")
+    text_file.write(f'The winner of the election, with {winner_vote_count} votes is: {winning_candidate}')
