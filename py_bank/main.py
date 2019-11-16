bec1ad64bf9024bea8e4e4373c791945590edad5
kstradinger <kevstradinger@gmail.com>
2019-11-16 10:21:01 -0600

    Added comments to walk the reader through my code and adjusted spacing to make code easier on the eyes

--- py_bank/main.py
+++ py_bank/main.py
@@ -1,44 +1,48 @@
+# Import Dependencies
 import os
 import csv
 import statistics
-
+# Create file Path
 csvpath = os.path.join('..','python-challenge', 'budget_data.csv')
-
+# import CSV and 
 with open(csvpath, newline="") as csv_file:
     csvreader = csv.reader(csv_file, delimiter = ',')
     print(csvreader)
     #read the header row first, 
     csv_header= next(csvreader)
     print(f'CSV Header: {csv_header}')
-    #set net/min/max profit equal to zero before beginging loop
-    net_profit = 0
-    max_profit = 0
-    min_profit = 0
-    profit_change = 0
-    profit_list =[]
-    profit_change_list = []
 
-    for row in csvreader:
-        #Define lists for Date & Row column
-        date = row[0]
-        profit= int(row[1])
-        profit_list.append(profit)
-        #Count the total months in data set
-        total_months=csvreader.line_num-1
-        #sum together all profit rows for net profit
-        net_profit= int(row[1]) + net_profit
-        #if then for max profit
-        if int(row[1]) > int(max_profit):
-            max_profit = row[1]
-        #if then for min profit
-        if int(row[1]) < int(min_profit):
-            min_profit = row[1]
+#set net/min/max profit equal to zero before beginging loop
+net_profit = 0
+max_profit = 0
+min_profit = 0
+profit_change = 0
+profit_list =[]
+profit_change_list = []
+
+# Open For loop
+for row in csvreader:
+    #Define lists for Date & Row column
+    date = row[0]
+    profit= int(row[1])
+    profit_list.append(profit)
+    #Count the total months in data set
+    total_months=csvreader.line_num-1
+    #sum together all profit rows for net profit
+    net_profit= int(row[1]) + net_profit
+    #if then for max profit
+    if int(row[1]) > int(max_profit):
+        max_profit = row[1]
+    #if then for min profit
+    if int(row[1]) < int(min_profit):
+        min_profit = row[1]
 
 # print(profit_list[0])
 list_length = len(profit_list)
 for i in range(list_length-1):
     profit_change = profit_list[i+1] - profit_list[i]
     profit_change_list.append(profit_change) 
+
 #Calculate average change
 average_change= round((statistics.mean(profit_change_list)),2)       
 
--- py_poll/main.py
+++ py_poll/main.py
@@ -12,42 +12,44 @@ with open(csv_path, newline="") as csv_file:
     csv_header= next(csv_reader)
     print(f'Header: {csv_header}')
 
-    #Define variables
-    total_votes=0
-    candidate_list = []
-    unique_candidate_list = []
-    maximum_votes = 0
-    #begin for loop
-    for row in csv_reader:
-        #Define columns as list variables
-        voter_id = row[0]
-        county = row[1]
-        candidate = str(row[2])
-        #count total # of voters
-        total_votes = total_votes + 1
-        #Identify candidates with votes 
-        candidate_list.append(row[2])
+#Define variables
+total_votes=0
+candidate_list = []
+unique_candidate_list = []
+maximum_votes = 0
+#begin for loop
+for row in csv_reader:
+    #Define columns as list variables
+    voter_id = row[0]
+    county = row[1]
+    candidate = str(row[2])
+    #count total # of voters
+    total_votes = total_votes + 1
+    #Identify candidates with votes 
+    candidate_list.append(row[2])
 
-   #make a list of unique candidate names for 
-    for x in candidate_list:
-            if x not in unique_candidate_list:
-                unique_candidate_list.append(x)   
-    #calculate vote count and percent for each candidate
-    kahn_votes = candidate_list.count("Khan")
-    kahn_pct= round((kahn_votes / total_votes)*100,2)
-    print(f'Khan: {kahn_votes} Votes ({kahn_pct}%)')
-        
-    correy_votes = candidate_list.count("Correy")
-    correy_pct=round((correy_votes/total_votes)*100,2)
-    print(f'Correy:{correy_votes} Votes ({correy_pct}%)')
-    
-    li_votes = candidate_list.count("Li")
-    li_pct=round((li_votes/total_votes)*100,2)
-    print(f'Li:{li_votes} Votes ({li_pct}%)')
-    
-    otooley_votes = candidate_list.count("O'Tooley")
-    otooley_pct = round((otooley_votes/total_votes)*100,2)
-    print(f"O'Tooley:{otooley_votes} Votes ({otooley_pct}%)")
+#make a list of unique candidate names for 
+for x in candidate_list:
+        if x not in unique_candidate_list:
+            unique_candidate_list.append(x)   
+#calculate vote count and percent for each candidate
+# Kahn
+kahn_votes = candidate_list.count("Khan")
+kahn_pct= round((kahn_votes / total_votes)*100,2)
+print(f'Khan: {kahn_votes} Votes ({kahn_pct}%)')
+# Correy
+correy_votes = candidate_list.count("Correy")
+correy_pct=round((correy_votes/total_votes)*100,2)
+print(f'Correy:{correy_votes} Votes ({correy_pct}%)')
+# Li
+li_votes = candidate_list.count("Li")
+li_pct=round((li_votes/total_votes)*100,2)
+print(f'Li:{li_votes} Votes ({li_pct}%)')
+# O'Tooley
+otooley_votes = candidate_list.count("O'Tooley")
+otooley_pct = round((otooley_votes/total_votes)*100,2)
+print(f"O'Tooley:{otooley_votes} Votes ({otooley_pct}%)")
+
 #Create a list of the vote ct for each candidate to make a winner variable
 vote_list= [kahn_votes, correy_votes,li_votes,otooley_votes]
 winner_vote_count = max(vote_list)
